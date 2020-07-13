331f4bb63809059f51d64adc347033036c76065b
Ian J Barker <ianjbark3r@gmail.com>
2020-07-13 11:39:45 -0500

    Animated CTA on scroll

--- src/components/layout/CTA.js
+++ src/components/layout/CTA.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ScrollAnimation from 'react-animate-on-scroll';
 import { Link } from 'react-router-dom';
 
 import { CTAdiv } from '../../Styles';
@@ -7,27 +8,34 @@ const CTA = () => {
   return (
     <CTAdiv className="row justify-content-center">
       <div className="col-lg-4 col-md-6 col-sm-8 col-9">
-        <h1 className="lead text-center">
-          <span>
-            Like what you see?
-          </span> 
-          <span>
-            <Link to="/contact">
-                Let's talk.
-            </Link>
-          </span>
-        </h1>
+        <ScrollAnimation animateIn='bounceInRight'>
+          <h1 className="lead text-center">
+            <span>
+              Like what you see?
+            </span> 
+            <span>
+              <Link to="/contact">
+                  Let's talk.
+              </Link>
+            </span>
+          </h1>
+        </ScrollAnimation>
         <hr/>
-        <h1 className="lead text-center">
-          <span>
-            Need more info?
-          </span> 
-          <span>
-            <Link to="/resume">
-              Check out my résumé.
-            </Link>
-          </span>
-        </h1>
+        <ScrollAnimation 
+          animateIn='bounceInRight' 
+          delay={1000}
+        >
+          <h1 className="lead text-center">
+            <span>
+              Need more info?
+            </span> 
+            <span>
+              <Link to="/resume">
+                Check out my résumé.
+              </Link>
+            </span>
+          </h1>
+        </ScrollAnimation>
       </div>
     </CTAdiv>
   )
