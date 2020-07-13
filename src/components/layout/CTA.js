307dc807697ad370d2a99127588df6bbb0aedebe
Ian J Barker <ianjbark3r@gmail.com>
2020-07-13 11:44:20 -0500

    Rolled back animate-on-scroll changes. Major issues with library on mobile

--- src/components/layout/CTA.js
+++ src/components/layout/CTA.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ScrollAnimation from 'react-animate-on-scroll';
 import { Link } from 'react-router-dom';
 
 import { CTAdiv } from '../../Styles';
@@ -8,34 +7,27 @@ const CTA = () => {
   return (
     <CTAdiv className="row justify-content-center">
       <div className="col-lg-4 col-md-6 col-sm-8 col-9">
-        <ScrollAnimation animateIn='bounceInRight'>
-          <h1 className="lead text-center">
-            <span>
-              Like what you see?
-            </span> 
-            <span>
-              <Link to="/contact">
-                  Let's talk.
-              </Link>
-            </span>
-          </h1>
-        </ScrollAnimation>
+        <h1 className="lead text-center">
+          <span>
+            Like what you see?
+          </span> 
+          <span>
+            <Link to="/contact">
+                Let's talk.
+            </Link>
+          </span>
+        </h1>
         <hr/>
-        <ScrollAnimation 
-          animateIn='bounceInRight' 
-          delay={1000}
-        >
-          <h1 className="lead text-center">
-            <span>
-              Need more info?
-            </span> 
-            <span>
-              <Link to="/resume">
-                Check out my résumé.
-              </Link>
-            </span>
-          </h1>
-        </ScrollAnimation>
+        <h1 className="lead text-center">
+          <span>
+            Need more info?
+          </span> 
+          <span>
+            <Link to="/resume">
+              Check out my résumé.
+            </Link>
+          </span>
+        </h1>
       </div>
     </CTAdiv>
   )
